6ef92d431fcc1e7d84a5901e7718b73b0427ada0
Saumyajit Samanta <saumyajit1996@gmail.com>
2017-02-03 19:13:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+   res.send('article1 is requested here');
+});
+app.get('/article-two',function(req,res){
+   res.send('article1 is requested here');
+});
+app.get('/article-three',function(req,res){
+   res.send('article1 is requested here');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
